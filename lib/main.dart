154d8864fe86f9b58984f8b6b9fa83e202b666d4
Aryan <hiaryan02@gmail.com>
2022-03-07 17:52:16 +0530

    🎉

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ class QuoteList extends StatefulWidget {
   @override
   _QuoteListState createState() => _QuoteListState();
 }
-
 class _QuoteListState extends State<QuoteList> {
   List<Quote> quotes=[
     Quote (author:'Aryan kumar',text:'is simply dummy text of the printing and typesetting industry'),
